93a064dda52dac76f1d39fa98b9ee128b9bdf1a8
uddeshya amit <uddeshyasrivastava05@gmail.com>
2023-11-25 18:09:46 +0530

    add delete functionality

--- index.js
+++ index.js
@@ -1,59 +1,75 @@
-let myLeads = []
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("ul-el")
-const remove = document.querySelectorAll(".del")
-const deleteBtn = document.getElementById("del-btn")
-const tabBtn = document.getElementById("tab-btn")
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("del-btn");
+const tabBtn = document.getElementById("tab-btn");
 
-const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads"))
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
+const deleteOne = (key) => {
+	myLeads = myLeads.filter((e, i) => i != key);
 
-deleteBtn.addEventListener("dblclick", ()=>{
-    localStorage.clear()
-    myLeads = []
-    render(myLeads)
-})
+	localStorage.setItem("myLeads", JSON.stringify(myLeads));
+	render();
+};
 
-tabBtn.addEventListener("click", function(){  
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-        render(myLeads)
-    })
-})
+deleteBtn.addEventListener("click", () => {
+	if (confirm("Do you really want to delete everything?")) {
+		localStorage.clear();
+		myLeads = [];
+		render();
+	}
+});
 
-if(leadsFromLocalStorage){
-    myLeads = leadsFromLocalStorage
-    render(myLeads)
+tabBtn.addEventListener("click", function () {
+	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+		myLeads.push(tabs[0].url);
+		localStorage.setItem("myLeads", JSON.stringify(myLeads));
+		render();
+	});
+});
+
+if (leadsFromLocalStorage) {
+	myLeads = leadsFromLocalStorage;
+	render();
 }
 
-inputBtn.addEventListener("click", function() {
-    if (inputEl.value != "" ){
-        myLeads.push(inputEl.value)
-    }
-    
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    render(myLeads)
-    inputEl.value = ""
-})
+inputBtn.addEventListener("click", saveInputValue);
+inputEl.addEventListener("keydown", (e) => {
+	if (e.key === "Enter") {
+		saveInputValue();
+	}
+});
 
-function render(leads) {
-    
-    let listItems = ""
-    for (let i = 0; i < leads.length; i++) {
-       
-            listItems += `
+function saveInputValue() {
+	if (inputEl.value != "") {
+		myLeads.push(inputEl.value);
+	}
+
+	localStorage.setItem("myLeads", JSON.stringify(myLeads));
+	render();
+	inputEl.value = "";
+}
+
+function render() {
+	let listItems = "";
+	for (let i = 0; i < myLeads.length; i++) {
+		listItems += `
             <li>
-                <a target='_blank' href='${leads[i]}'>
-                    ${leads[i]}
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
                 </a>
-                  
+                <button class="del">X</button>                 
             </li>
-        `
-       
-    }
-    ulEl.innerHTML = listItems  
-}
+        `;
+	}
+	ulEl.innerHTML = listItems;
 
+	document.querySelectorAll(".del").forEach((button, index) => {
+		button.addEventListener("click", () => {
+			deleteOne(index);
+		});
+	});
+}
 
